4a41939ab2abe370de7eb7b8b74e64df7cdedc41
compace <compace@ya.ru>
2014-12-02 18:27:12 +0300

    Добавлено выделение цветом поручений и отчетов по поручению в зависимости от срока (статуса)

---
+++ AS-GSMO-Solution/GradSovetPages/Layouts/GradSovetPages/Scripts/csr/renderAssignmentMVKList.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    function init() {
+        SPClientTemplates.TemplateManager.RegisterTemplateOverrides({
+            OnPostRender: OnPostRender,
+        });
+    }
+
+    function OnPostRender(ctx) {
+		if (!ctx.listUrlDir.endsWith("/Lists/AssignmentMVKList"))
+			return;
+		
+		var rows = ctx.ListData.Row;
+		for (var i = 0; i < rows.length; i++)
+		{
+			var status = rows[i]["AssignmentStatusMVK"];
+			var color = null;
+			
+			if (status === "Срок истек")
+				color = "#daa";
+			else if (status === "Исполнено")
+				color = "#ada";
+			
+			if (color) {
+				var rowElementId = GenerateIIDForListItem(ctx, rows[i]);
+				var tr = document.getElementById(rowElementId);
+				tr.style.backgroundColor = color;
+			}
+		}
+    }
+
+    SP.SOD.executeOrDelayUntilScriptLoaded(function () {
+        init();
+    }, 'clienttemplates.js');
+})();
---
+++ AS-GSMO-Solution/GradSovetPages/Layouts/GradSovetPages/Scripts/csr/renderMVKReportList.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    function init() {
+        SPClientTemplates.TemplateManager.RegisterTemplateOverrides({
+            OnPostRender: OnPostRender,
+        });
+    }
+
+    function OnPostRender(ctx) {
+		if (!ctx.listUrlDir)
+			return;
+ 		if (!ctx.listUrlDir.endsWith("/Lists/ReportMVKList"))
+			return;
+		
+		var now = new Date().getTime();
+		var millisecondsPerDay = 1000 * 60 * 60 * 24;
+		
+		var rows = ctx.ListData.Row;
+		for (var i = 0; i < rows.length; i++) {
+			var dateString = rows[i]["_x041f__x043e__x0440__x0443__x04"];
+			if (dateString) {
+				var date = Date.parseLocale(dateString, "dd.MM.yyyy");
+				date.setTime(date.getTime() - millisecondsPerDay * 3);
+				if (now >= date.getTime()) {
+					var rowElementId = GenerateIIDForListItem(ctx, rows[i]);
+					var tr = document.getElementById(rowElementId);
+					tr.style.backgroundColor = "#daa";
+				}
+			}
+		}
+    }
+
+    SP.SOD.executeOrDelayUntilScriptLoaded(function () {
+        init();
+    }, 'clienttemplates.js');
+})();
